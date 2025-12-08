54e23c7a5c022c9f39ea8a8d5a2d9b86d39d664b
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-08 18:28:52 +0900

    feat: 회원가입 이메일인증 화면 디자인 구현

---
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/SetEmail.kt
@@ -0,0 +1,73 @@
+package com.example.healthmeconverttocomposablecode.presentation.auth.signup
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.healthmeconverttocomposablecode.R
+import com.example.healthmeconverttocomposablecode.presentation.component.AuthCodeInputField
+import com.example.healthmeconverttocomposablecode.presentation.component.EmailAuthInputField
+import com.example.healthmeconverttocomposablecode.presentation.component.MediumButton
+import com.example.healthmeconverttocomposablecode.ui.AppColors
+import com.example.healthmeconverttocomposablecode.ui.AppFonts
+
+@Composable
+fun SetEmail() {
+    Box{
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = AppColors.mainColor)
+                .padding(horizontal = 25.dp)
+            , verticalArrangement = Arrangement.Center
+
+        ) {
+            Spacer(modifier = Modifier.height(142.dp))
+            Text(
+                "Enter\nyour Email",
+                modifier = Modifier.padding(start = 13.dp),
+                color = AppColors.white,
+                fontSize = 32.sp,
+                fontFamily = AppFonts.gmarketSans,
+                lineHeight = 42.sp
+            )
+            Spacer(modifier = Modifier.height(21.dp))
+            EmailAuthInputField("이메일", "healtm@gmail.com")
+            Spacer(modifier = Modifier.height(13.dp))
+            AuthCodeInputField("인증코드")
+            Spacer(modifier = Modifier.height(102.dp))
+            Box(modifier = Modifier.padding(horizontal = 48.dp)) {
+                MediumButton("다음")
+            }
+            Spacer(modifier = Modifier.height(224.dp))
+        }
+        Image(
+            painter = painterResource(R.drawable.under_background),
+            contentDescription = "배경",
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SetEmailPreview() {
+    SetEmail()
+}
