69ce6f8be93ea5beb220e6be72e17bda71d14d80
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-08 19:28:26 +0900

    feat: 정규식으로 사용자가 입력한 텍스트가 이메일인지 확인하고 이메일일 경우 버튼 활성화하는 기능 추가

--- app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/SetEmail.kt
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/SetEmail.kt
@@ -12,6 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -27,6 +31,9 @@ import com.example.healthmeconverttocomposablecode.ui.AppFonts
 
 @Composable
 fun SetEmail() {
+    var isCodeFieldVisible by remember { mutableStateOf(false) }
+
+
     Box{
         Column(
             modifier = Modifier
@@ -46,9 +53,15 @@ fun SetEmail() {
                 lineHeight = 42.sp
             )
             Spacer(modifier = Modifier.height(21.dp))
-            EmailAuthInputField("이메일", "healtm@gmail.com")
+            EmailAuthInputField("이메일", "health@gmail.com", onClick = {isCodeFieldVisible=true})
             Spacer(modifier = Modifier.height(13.dp))
-            AuthCodeInputField("인증코드")
+            if(isCodeFieldVisible){
+                AuthCodeInputField("인증코드")
+            }
+            else{
+                Spacer(modifier=Modifier.height(82.dp))
+            }
+
             Spacer(modifier = Modifier.height(102.dp))
             Box(modifier = Modifier.padding(horizontal = 48.dp)) {
                 MediumButton("다음")
