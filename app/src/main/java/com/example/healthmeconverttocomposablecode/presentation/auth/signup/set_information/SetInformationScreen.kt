bf6307fa74777e870850b52f249d6e3d92661d67
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-15 03:39:14 +0900

    feat: 회원가입 정보 입력 화면 중간저장

---
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/set_information/SetInformationScreen.kt
@@ -0,0 +1,21 @@
+package com.example.healthmeconverttocomposablecode.presentation.auth.signup.set_information
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SetInformationScreen() {
+    Column(modifier= Modifier) {
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SetInformationScreenPreview() {
+    SetInformationScreen()
+}
