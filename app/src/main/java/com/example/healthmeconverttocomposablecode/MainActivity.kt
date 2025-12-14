504ea150b739d7606e2d3b41f63b2a846b28e7c6
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-14 15:25:56 +0900

    feat: 내비게이션 기능의 시작점 `NavigationRoot` 추가

--- app/src/main/java/com/example/healthmeconverttocomposablecode/MainActivity.kt
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.healthmeconverttocomposablecode.core.routing.NavigationRoot
 import com.example.healthmeconverttocomposablecode.ui.theme.HealthMeConvertToComposableCodeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HealthMeConvertToComposableCodeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            NavigationRoot()
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HealthMeConvertToComposableCodeTheme {
-        Greeting("Android")
     }
 }
