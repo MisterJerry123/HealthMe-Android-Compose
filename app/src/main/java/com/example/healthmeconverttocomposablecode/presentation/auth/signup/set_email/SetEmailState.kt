3069811310b2d04104d7382d2874307335a7aa1d
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-14 22:37:46 +0900

    feat: 이메일 설정 화면 상태를 관리하는 SetEmailState 데이터 클래스 구현

---
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/presentation/auth/signup/set_email/SetEmailState.kt
@@ -0,0 +1,15 @@
+package com.example.healthmeconverttocomposablecode.presentation.auth.signup.set_email
+
+data class SetEmailState(
+    val email: String = "",
+    val isCodeFieldVisible: Boolean = false,
+    val authCode: String = "",
+    val isAuthCodeCheckButton: Boolean = false,
+    val isEnableResendAuthCodeButton: Boolean = false,
+    val isNextButtonEnabled: Boolean = false,
+    val isRequestAuthCodeButtonEnabled: Boolean = false,
+    val remainingTime: Int = 300,
+    val isTextFieldEnabled: Boolean = true,
+    val isAuthCodeFieldEnabled: Boolean = false,
+    val isCheckAuthCodeButtonEnabled: Boolean = false
+)
