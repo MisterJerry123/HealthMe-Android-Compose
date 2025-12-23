be13ddffa1850da0967c7e3082b32653332ca7a4
MisterJerry123 <misterjerry12345@gmail.com>
2025-12-23 18:35:21 +0900

    chore: MainActivity edge-to-edge 설정 비활성화

--- app/src/main/java/com/example/healthmeconverttocomposablecode/MainActivity.kt
+++ app/src/main/java/com/example/healthmeconverttocomposablecode/MainActivity.kt
@@ -17,7 +17,7 @@ import com.example.healthmeconverttocomposablecode.ui.theme.HealthMeConvertToCom
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             NavigationRoot()
 
